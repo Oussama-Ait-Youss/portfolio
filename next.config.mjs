6724db87a46cd15083bf0d3e18e86f43c54305ac
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-07-31 19:13:50 +0200

    feat: remove the next framwork icon

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  devIndicators: false,
+  output: 'export',
+  // If your repository name is "my-portfolio", uncomment and set this:
+  // assetPrefix: '/my-portfolio/',
+  // basePath: '/my-portfolio',
+  images: {
+    unoptimized: true, // Required for static export images
+  },
 };
 
 export default nextConfig;
