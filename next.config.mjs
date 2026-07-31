0fba87e5e43083d0d989264fba307924fd110b48
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-07-31 19:24:14 +0200

    feat: configure the next.config.mjs for deployement

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,9 @@
 const nextConfig = {
   /* config options here */
   devIndicators: false,
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
   output: 'export',
-  // If your repository name is "my-portfolio", uncomment and set this:
-  // assetPrefix: '/my-portfolio/',
-  // basePath: '/my-portfolio',
   images: {
     unoptimized: true, // Required for static export images
   },
